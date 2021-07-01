9f5c1413868d84a56501497307d0f7dd78d5af4d
Andrey Bezrukov <andruxa.ic@gmail.com>
2021-07-01 11:42:08 +0400

    Выполнено 3 ДЗ

---
+++ src/HW5/Employee.java
@@ -0,0 +1,28 @@
+package HW5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void showInfo() {
+        System.out.printf("Сотрудник [ФИО: %s, Должность: %s, e-mail: %s, Номер телефона: %s, Зарплата: %d, Возраст: %d]\n", name, position, email, phone, salary, age);
+    }
+
+}
---
+++ src/HW5/Main.java
@@ -0,0 +1,19 @@
+package HW5;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new Employee("Алексей Иванов", "Уборщик", "aliv@mail.ru", "89273002020", 10000, 65),
+                new Employee("Иван Алексеев", "Бухгалтер", "ival@mail.ru", "89273002021", 30000, 35),
+                new Employee("Марина Петрова", "Директор", "marina@mail.ru", "89273002022", 100000, 25),
+                new Employee("Елена Пупкина", "Программист", "elpup@mail.ru", "89273002023", 50000, 31),
+                new Employee("Виктор Сидоров", "Водитель", "visid@mail.ru", "89273002024", 20000, 42),
+        };
+
+        for (Employee employee : employees) {
+            if (employee.getAge() >= 40) {
+                employee.showInfo();
+            }
+        }
+    }
+}
